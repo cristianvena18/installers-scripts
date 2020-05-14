a26c0531893e6b076aed18f240cefb74bb91eeec
cristianvena18 <cristiandamianvena@gmail.com>
2020-05-14 12:50:55 -0300

    added echos

--- installer.sh
+++ installer.sh
@@ -1,42 +1,59 @@
 # Actualizar el sistema
-sudo apt update && sudo apt upgrade -y
+echo "UPDATING SYSTEM..."
+sudo apt update -y && sudo apt upgrade -y
 
 # Installer google chromium
-sudo apt-get install chromium-browser
+echo "INSTALLING CHROMIUN..."
+sudo apt-get install -y chromium-browser
 
 # install snap
-sudo apt-get install snapd
+echo "INSTALLING SNAP..."
+sudo apt-get install -y snapd
 
 # install vscode
+echo "INSTALLING VSCODE..."
 sudo snap install --classic code
 
 # install phpstorm
+echo "INSTALLING PHPSTORM..."
 sudo snap install --classic phpstorm
 
 # install webstorm
+echo "INSTALLING WEBSTORM..."
 sudo snap install --classic webstorm
 
+
+echo "INSTALLING SLACK..."
 sudo snap install slack --classic
 
 # cURL
+echo "INSTALLING CURL..."
 sudo apt install -y curl
 
+# workbench
+echo "INSTALLING WORKBENCH..."
+sudo apt install -y mysql-workbench
+
 # Node
+echo "INSTALLING NODE..."
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt install -y nodejs
 
 # Yarn
+echo "INSTALLING YARN..."
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt-get update && sudo apt-get install -y yarn
 
 # Lerna
-# sudo yarn global add lerna
+echo "INSTALLING LERNA..."
+sudo yarn global add lerna
 
 # PM2 - Server only
 # sudo yarn global add pm2
 
 # Docker
+echo "INSTALLING DOCKER..."
 sudo apt install -y \
     apt-transport-https \
     ca-certificates \
@@ -55,7 +72,7 @@ sudo usermod -aG docker $USER
 echo "Log out and log back in so that your group membership is re-evaluated."
 
 # Docker-Compose
-
+echo "INSTALLING DOCKER..."
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
 sudo chmod +x /usr/local/bin/docker-compose
